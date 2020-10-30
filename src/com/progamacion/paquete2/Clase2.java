79d24be4f73ac35bd20f07cf18df65828b5186bf
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2020-10-30 11:00:51 +0100

    Clase2 de paquete2 subido desde IDEA

---
+++ src/com/progamacion/paquete2/Clase2.java
@@ -0,0 +1,13 @@
+package com.progamacion.paquete2;
+
+/**
+ * Creado por @autor: Usuario
+ * El 30/10/2020
+ **/
+public class Clase2 {
+    public static void main(String[] args) {
+        System.out.println("Clase ejemplo2");
+        System.out.println("Modificación para comprobar");
+    }
+
+}
