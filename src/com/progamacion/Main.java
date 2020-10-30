901ab64276d4ebc71a0d2c997a1c511ab92d8413
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2020-10-30 11:26:38 +0100

    Commit a la clase main con modificación

--- src/com/progamacion/Main.java
+++ src/com/progamacion/Main.java
@@ -1,8 +1,11 @@
 package com.progamacion;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Esto es la prueba de ejercicio para proyecto COD");
+        System.out.println("Otra de las pruebas de modificación y subida");
     }
 }
