d2f9a8bad9d8c3381cc8ffa16b1feee5dc20bad2
JSampereVázquez <jsamperevazquez@danielcastelao.org>
2020-10-30 11:47:44 +0100

    Clase1 del paquete1 subido desde IDEA

---
+++ src/com/progamacion/paquete1/Clase1.java
@@ -0,0 +1,12 @@
+package com.progamacion.paquete1;
+
+/**
+ * Creado por @autor: Usuario
+ * El 30/10/2020
+ **/
+public class Clase1 {
+
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba para ver clase diferente");
+    }
+    }
